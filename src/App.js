426a5c488d33afdcd583003fd682a3d567b06033
Tianjian Wang <wangtjwork@gmail.com>
2018-01-17 11:51:35 -0600

    orgainze bookshelves

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import SearchPage from './SearchPage'
 import ShowBooks from './ShowBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books: books
+      });
+      console.log(books);
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ShowBooks />
+          <ShowBooks
+            books={this.state.books}
+          />
         )} />
         <Route path="/search" render={() => (
           <SearchPage />
